64f85c714ebc719d57ab52f7e84758282b28ee1b
AndresEstevez <100358857@alumnos.uc3m.es>
2020-02-25 20:37:59 +0100

    Trying to solve task4b

--- task4b.py
+++ task4b.py
@@ -5,8 +5,8 @@ import torchvision
 import torch
 import numpy as np
 image = Image.open("images/zebra.jpg")
-print("Image shape:", image.size)
-#holaaaaaaaa
+print("Image shape before transformation:", image.size)
+#holaaaaAA
 model = torchvision.models.resnet18(pretrained=True)
 print(model)
 first_conv_layer = model.conv1
@@ -20,7 +20,7 @@ image_transform = torchvision.transforms.Compose([
     torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 image = image_transform(image)[None]
-print("Image shape:", image.shape)
+print("Image shape after transformation:", image.shape)
 
 activation = first_conv_layer(image)
 print("Activation shape:", activation.shape)
@@ -45,5 +45,29 @@ def torch_image_to_numpy(image: torch.Tensor):
     image = np.moveaxis(image, 0, 2)
     return image
 
+def plot_kernels(tensor, num_cols=6):
+    if not tensor.ndim==4:
+        raise Exception("assumes a 4D tensor")
+    if not tensor.shape[-1]==3:
+        raise Exception("last dim needs to be 3 to plot")
+    num_kernels = tensor.shape[0]
+    num_rows = 1+ num_kernels // num_cols
+    fig = plt.figure(figsize=(num_cols,num_rows))
+    for i in range(tensor.shape[0]):
+        ax1 = fig.add_subplot(num_rows,num_cols,i+1)
+        ax1.imshow(tensor[i])
+        ax1.axis('off')
+        ax1.set_xticklabels([])
+        ax1.set_yticklabels([])
+
+    plt.subplots_adjust(wspace=0.1, hspace=0.1)
+    plt.show()
+
+weights_tensor = first_conv_layer.weight.data
+weights_tensor = torch_image_to_numpy(weights_tensor)
+plot_kernels(weights_tensor, 6)
+
+
 
 indices = [14, 26, 32, 49, 52]
+
