a772820caeaa329cd314fc3675b181b6d8b9367f
AndresEstevez <100358857@alumnos.uc3m.es>
2020-02-25 21:01:49 +0100

    ...

--- task4b.py
+++ task4b.py
@@ -65,6 +65,7 @@ def plot_kernels(tensor, num_cols=6):
 
 weights_tensor = first_conv_layer.weight.data
 print("Weights_tensor shape:", weights_tensor.shape)
+print("Weights_tensor filter 0 shape:", weights_tensor[0].shape)
 exit()
 weights_tensor = torch_image_to_numpy(weights_tensor)
 plot_kernels(weights_tensor, 6)
