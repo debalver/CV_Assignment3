d3b4bf3240b9e7fe2eabbbcaa2968f4dcbc6bc2b
AndresEstevez <100358857@alumnos.uc3m.es>
2020-02-26 02:34:01 +0100

    Task4c in progress...

--- task4c.py
+++ task4c.py
@@ -69,4 +69,5 @@ def plot_filters(weights_tensor: torch.Tensor, filter_activation: torch.Tensor, 
 #indices = [14, 26, 32, 49, 52]
 #plot_filters(weights_tensor, activation, indices, "filters_activation_task4b")
 print("------------------------------------------------------------------------------------------------")
-print(model.children)
+activation = model.children()(image)
+print("Shape of activation from last Conv. Layer: ", activation.shape)
